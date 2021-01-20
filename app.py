664379341e2be19f06164b90246b54e8f5984b7a
Gerardin <niveadha@gmail.com>
2021-01-20 23:06:57 +0530

    Changed the styles. Renamed some files. Added favicon.ico Added base.html

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
 
-
-# @app.route('/<name>')
-# def hello_name(name):
-#     return "Hello {}!".format(name)
 
 if __name__ == '__main__':
     app.run(debug=True)
