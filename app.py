2acd80489ffea0d59df77b455862449fd46b4263
Gerardin <niveadha@gmail.com>
2021-01-20 21:23:49 +0530

    Coded the html file and added styles.css

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask
+from flask import Flask,render_template
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+# @app.route('/<name>')
+# def hello_name(name):
+#     return "Hello {}!".format(name)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
