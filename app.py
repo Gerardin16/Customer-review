0fa10aeccb7c9f5e77bc1a72af44317a08183653
Gerardin <niveadha@gmail.com>
2021-01-24 00:25:28 +0530

    Added the WTforms implementation Deleted unnecesaary files

--- app.py
+++ app.py
@@ -1,11 +1,48 @@
-from flask import Flask,render_template,request
+
+from flask import Flask,render_template,request,url_for,flash,redirect
+from flask_wtf import FlaskForm
+from wtforms import (Form,StringField, BooleanField, DateTimeField,
+                     RadioField,SelectField,TextField,
+                     TextAreaField,SubmitField)
+from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'mysecretkey'
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+# WTForm Class
+class ReviewForm(FlaskForm):
+
+   #Get the fields to be filled in the form
+    name = StringField('Name*: ',validators=[DataRequired()])
+    # product = RadioField('Please choose your product: ', choices=[('opt-1', 'Samsung'), ('opt-2', 'Oneplus'),
+    #                                                             ('opt-3', 'Vivo 5'), ('opt-4', 'Redmi 6')])
+
+    product = SelectField(u'Please choose your product:',
+                          choices=[('opt-1', 'Samsung'), ('opt-2', 'Oneplus'),
+                                   ('opt-3', 'Vivo 5'), ('opt-4', 'Redmi 6')])
+    review = TextAreaField()
+    submit = SubmitField('Submit your review')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    # Create instance of the form.
+    form = ReviewForm()
+    # If the form is valid on submission 
+    if form.validate_on_submit() and request.method =='POST':
+        #Display a success message
+        flash("Your review has been submitted succesfully")
+        # return redirect(url_for("thankyou"))
+
+
+    return render_template('index.html', form=form)
+
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
 
 
 if __name__ == '__main__':
