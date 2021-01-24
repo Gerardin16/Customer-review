a72c674ffe312a8d81211954bca5e9851a52d550
Gerardin <niveadha@gmail.com>
2021-01-25 00:25:09 +0530

    Added a csv file Made changes to index.html to display the form

--- app.py
+++ app.py
@@ -1,48 +1,60 @@
 
-from flask import Flask,render_template,request,url_for,flash,redirect
+from flask import Flask,render_template,request
 from flask_wtf import FlaskForm
 from wtforms import (Form,StringField, BooleanField, DateTimeField,
                      RadioField,SelectField,TextField,
                      TextAreaField,SubmitField)
 from wtforms.validators import DataRequired
-from flask_bootstrap import Bootstrap
+import pandas as pd
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'mysecretkey'
+app.config['SECRET_KEY'] = 'mykey'
+
+# to read csv file named "samplee" 
+a = pd.read_csv("Reviews.csv") 
+  
+# to save as html file 
+# named as "Table" 
+a.to_html("Table.htm") 
+  
+# assign it to a  
+# variable (string) 
+html_file = a.to_html() 
+
+# with open('Reviews.csv') as reviews_file:
+#     csv_reader= csv.reader(reviews_file, delimiter=',')
+#     line_count=0;
 
 # WTForm Class
 class ReviewForm(FlaskForm):
 
    #Get the fields to be filled in the form
     name = StringField('Name*: ',validators=[DataRequired()])
-    # product = RadioField('Please choose your product: ', choices=[('opt-1', 'Samsung'), ('opt-2', 'Oneplus'),
-    #                                                             ('opt-3', 'Vivo 5'), ('opt-4', 'Redmi 6')])
-
-    product = SelectField(u'Please choose your product:',
+    product = SelectField(u'Choose your product:',
                           choices=[('opt-1', 'Samsung'), ('opt-2', 'Oneplus'),
-                                   ('opt-3', 'Vivo 5'), ('opt-4', 'Redmi 6')])
+                                   ('opt-3', 'Vivo5'), ('opt-4', 'Redmi6')])
     review = TextAreaField()
     submit = SubmitField('Submit your review')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-
+    # Set the breed to a boolean False.
+    # So we can use it in an if statement in the html.
     # Create instance of the form.
     form = ReviewForm()
-    # If the form is valid on submission 
-    if form.validate_on_submit() and request.method =='POST':
-        #Display a success message
-        flash("Your review has been submitted succesfully")
-        # return redirect(url_for("thankyou"))
-
-
-    return render_template('index.html', form=form)
-
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
+    # If the form is valid on submission (we'll talk about validation next)
+    if form.validate_on_submit() :
+        # Grab the data from the breed on the form.
+        name= form.name.data
+        product=form.product.data
+        review=form.review.data
+        # Reset the form's breed data to be False
+        form.name.data = ''
+        form.product.data='Samsung'
+        form.review.data=''
+    return render_template('index.html', form=form,)
 
 
 if __name__ == '__main__':
